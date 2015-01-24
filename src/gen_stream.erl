af3bae4b0f2f7867e77d7a10a0be3037d57b9838
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2015-01-24 14:25:25 +0000

    bare gen_stream

---
+++ src/gen_stream.erl
@@ -0,0 +1,231 @@
+%%% -*- erlang -*-
+%%%-------------------------------------------------------------------
+%%% @author Evangelos Pappas <epappas@evalonlabs.com>
+%%% @copyright (C) 2015, evalonlabs
+%%% The MIT License (MIT)
+%%%
+%%% Copyright (c) 2015 Evangelos Pappas
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in all
+%%% copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%%% SOFTWARE.
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(gen_stream).
+-author("epappas").
+
+-behaviour(gen_fsm).
+
+%% API
+-export([
+  start_link/0,
+  start_link/4,
+  pipe/1,
+  pipe/2
+]).
+
+%% gen_fsm callbacks
+-export([
+  init/1,
+  state_name/2,
+  state_name/3,
+  handle_event/3,
+  handle_sync_event/4,
+  handle_info/3,
+  terminate/3,
+  code_change/4
+]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {
+  is_paused = false,
+  is_stoped = false,
+  is_reading = false,
+  is_writing = false,
+  has_started = false,
+  pipes = [],
+  buffer = []
+}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+-spec(start_link() -> {ok, pid()} | ignore | {error, Reason :: term()}).
+start_link(Name, Mod, Args, Options) ->
+  gen_fsm:start_link(Name, Mod, Args, Options).
+
+-spec(start_link() -> {ok, pid()} | ignore | {error, Reason :: term()}).
+start_link() ->
+  gen_fsm:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+-spec(pipe(any(), any()) -> {ok, pid()}).
+pipe(Mod) -> pipe(Mod, []).
+
+-spec(pipe(any(), any()) -> {ok, pid()}).
+pipe(Mod, Args) -> {ok, ?MODULE}.
+
+%%%===================================================================
+%%% gen_fsm callbacks
+%%%===================================================================
+
+-spec(init(Args :: term()) ->
+  {ok, StateName :: atom(), StateData :: #state{}} |
+  {ok, StateName :: atom(), StateData :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
+init([]) ->
+  {ok, state_name, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% There should be one instance of this function for each possible
+%% state name. Whenever a gen_fsm receives an event sent using
+%% gen_fsm:send_event/2, the instance of this function with the same
+%% name as the current state name StateName is called to handle
+%% the event. It is also called if a timeout occurs.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(state_name(Event :: term(), State :: #state{}) ->
+  {next_state, NextStateName :: atom(), NextState :: #state{}} |
+  {next_state, NextStateName :: atom(), NextState :: #state{},
+    timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+state_name(_Event, State) ->
+  {next_state, state_name, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% There should be one instance of this function for each possible
+%% state name. Whenever a gen_fsm receives an event sent using
+%% gen_fsm:sync_send_event/[2,3], the instance of this function with
+%% the same name as the current state name StateName is called to
+%% handle the event.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(state_name(Event :: term(), From :: {pid(), term()},
+    State :: #state{}) ->
+  {next_state, NextStateName :: atom(), NextState :: #state{}} |
+  {next_state, NextStateName :: atom(), NextState :: #state{},
+    timeout() | hibernate} |
+  {reply, Reply, NextStateName :: atom(), NextState :: #state{}} |
+  {reply, Reply, NextStateName :: atom(), NextState :: #state{},
+    timeout() | hibernate} |
+  {stop, Reason :: normal | term(), NewState :: #state{}} |
+  {stop, Reason :: normal | term(), Reply :: term(),
+    NewState :: #state{}}).
+state_name(_Event, _From, State) ->
+  Reply = ok,
+  {reply, Reply, state_name, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_fsm receives an event sent using
+%% gen_fsm:send_all_state_event/2, this function is called to handle
+%% the event.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_event(Event :: term(), StateName :: atom(),
+    StateData :: #state{}) ->
+  {next_state, NextStateName :: atom(), NewStateData :: #state{}} |
+  {next_state, NextStateName :: atom(), NewStateData :: #state{},
+    timeout() | hibernate} |
+  {stop, Reason :: term(), NewStateData :: #state{}}).
+handle_event(_Event, StateName, State) ->
+  {next_state, StateName, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a gen_fsm receives an event sent using
+%% gen_fsm:sync_send_all_state_event/[2,3], this function is called
+%% to handle the event.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_sync_event(Event :: term(), From :: {pid(), Tag :: term()},
+    StateName :: atom(), StateData :: term()) ->
+  {reply, Reply :: term(), NextStateName :: atom(), NewStateData :: term()} |
+  {reply, Reply :: term(), NextStateName :: atom(), NewStateData :: term(),
+    timeout() | hibernate} |
+  {next_state, NextStateName :: atom(), NewStateData :: term()} |
+  {next_state, NextStateName :: atom(), NewStateData :: term(),
+    timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewStateData :: term()} |
+  {stop, Reason :: term(), NewStateData :: term()}).
+handle_sync_event(_Event, _From, StateName, State) ->
+  Reply = ok,
+  {reply, Reply, StateName, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_fsm when it receives any
+%% message other than a synchronous or asynchronous event
+%% (or a system message).
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_info(Info :: term(), StateName :: atom(),
+    StateData :: term()) ->
+  {next_state, NextStateName :: atom(), NewStateData :: term()} |
+  {next_state, NextStateName :: atom(), NewStateData :: term(),
+    timeout() | hibernate} |
+  {stop, Reason :: normal | term(), NewStateData :: term()}).
+handle_info(_Info, StateName, State) ->
+  {next_state, StateName, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_fsm when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_fsm terminates with
+%% Reason. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(terminate(Reason :: normal | shutdown | {shutdown, term()}
+| term(), StateName :: atom(), StateData :: term()) -> term()).
+terminate(_Reason, _StateName, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(code_change(OldVsn :: term() | {down, term()}, StateName :: atom(),
+    StateData :: #state{}, Extra :: term()) ->
+  {ok, NextStateName :: atom(), NewStateData :: #state{}}).
+code_change(_OldVsn, StateName, State, _Extra) ->
+  {ok, StateName, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
